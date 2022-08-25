c5f4526d15bf09e07ba34cac1068b620e01f934a
MYKOLA SHEREMET <artem.vazovskin@gmail.com>
2022-08-25 13:12:09 +0300

    fix lightining

--- src/js/libraries.js
+++ src/js/libraries.js
@@ -1,12 +1,12 @@
 import defaultPoster from '../img/cinema320.jpg';
-
+import defaultPosterMob from '../img/cinema480.jpg';
+import defaultPosterTab from '../img/cinema768.jpg';
+import defaultPosterDesc from '../img/cinema1280.jpg';
 
 const btnLibrary = document.querySelector('.link-library');
 const listOfFilmMain = document.querySelector('.films_list');
 const divConatiner = document.querySelector('.container-library');
-console.log(btnLibrary);
 
-console.log(defaultPoster);
 
 btnLibrary.addEventListener('click', checkcontains);
 
@@ -16,21 +16,14 @@ let localstorag = [];
 function checkcontains(e) {
     if (localstorag.length === 0) {
         listOfFilmMain.innerHTML = '';
-        renderStartsShowpictureNoting();
+        renderIfLibraryEmpty();
+       }
 }
 
-    console.log(e);
-}
-
-console.log("artem")
-
-
-function renderStartsShowpictureNoting() {
+function renderIfLibraryEmpty() {
     const rezult =
-        `
-        <img class="images-cinema" src="${defaultPoster}" alt="cinema">
+        `<img class="images-cinema" src="${defaultPoster}" alt="cinema">
         <p class="p-library"> Sorry, but you haven't added anything to your library yet </p>
-        
         `
-    divConatiner.insertAdjacentHTML('beforeend', rezult)
+        divConatiner.insertAdjacentHTML('beforeend', rezult)
 }
