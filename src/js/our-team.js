59dd09b5370f44cc64268142ed8854ced71a6b01
Victoria Bulakh <bulakh.victoria29@gmail.com>
2022-08-25 13:07:50 +0300

    Add team markup

--- src/js/our-team.js
+++ src/js/our-team.js
@@ -6,18 +6,47 @@ const teamMarkup = createMarkup(teamItems);
 teamItemContainer.insertAdjacentHTML("beforeend", teamMarkup);
 
 function createMarkup(teamItems) {
-    return teamItems.map(({preview, name, description, role }) => {
+    return teamItems.map(({preview, name, description, role, quote}) => {
         return `
         <li class="glide__slide">
-            <img class="team__image" src="${preview}" alt="${description}">
+            <div class="team__img-thumb"><img class="team__image" src="${preview}" alt="${description}"></div>
             <h3 class="team__subtitle">${name}</h3>
-            <p class="team__item"> ${role}</>
+            <p class="team__item"> ${role}</p>
+            <p class="team__item"> ${quote}</p>
         </li>`;
     })
     .join("");
 }
 
+const glideOptions = {
+    type: 'carousel',
+    startAt: 0,
+    perView: 1,
+    autoplay: 3000,
+    keyboard: true,
+    hoverpause: true,
+    dots: '#dots',
+    draggable: true,
+}
+
+export const glide = new Glide('.glide', glideOptions).mount();
 
 
-new Glide('.glide').mount()
+var nextButton = document.querySelector('#next');
+var prevButton = document.querySelector('#prev');
+
+nextButton.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  glide.go('>');
+})
+
+prevButton.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  glide.go('<');
+})
+
+glide.mount();
+
 
