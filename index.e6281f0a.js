40a588ebace852dfda803e982626269a8419aff2
MykolaySheremet <97753142+MykolaySheremet@users.noreply.github.com>
2022-08-26 23:21:49 +0000

    Deploying to gh-pages from @ MykolaySheremet/project--JS-09-group@e3903b702344c7839caf666d85f51e57a05b61c1 🚀

---
+++ index.e6281f0a.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".preloader");window.addEventListener("load",(function(){e.classList.add("visually-hidden"),setTimeout((function(){e.remove()}),1e3)}))}();
+//# sourceMappingURL=index.e6281f0a.js.map
---
+++ index.f21230ba.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".preloader");window.addEventListener("load",(function(){e.classList.add("visually-hidden"),setTimeout((()=>{e.remove()}),1e3)}));
+//# sourceMappingURL=index.f21230ba.js.map
