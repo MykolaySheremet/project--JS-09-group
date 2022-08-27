4d343d716a2924d85e830d3b9d549711dd03a0d7
MYKOLA SHEREMET <artem.vazovskin@gmail.com>
2022-08-27 15:31:56 +0300

    add logical to render wached wilm

--- src/js/library-wached-quee-film.js
+++ src/js/library-wached-quee-film.js
@@ -1,9 +1,23 @@
 import { checkContains } from "./libraries"
 
+import axios from "axios"
+
+const KEY = '2acc48bc8101b89794229029120e4b70'
+const BASE_URL = 'https://api.themoviedb.org/3/'
+
+import { getMovieDetails } from "./fetchFilms"
+
+console.log(getMovieDetails);
+
 const btnWached = document.querySelector('.library-first')
 const btnQuue = document.querySelector('.library-second')
+const gallery = document.querySelector('.films_list')
 
-const localStorageWached = localStorage.getItem('watchedFilms')
+
+
+// console.log(localStorageWached)
+
+// console.log(localStorageWached.length);
 
 
 btnWached.addEventListener('click', rendercards);
@@ -11,27 +25,78 @@ btnWached.addEventListener('click', rendercards);
 
 
 function rendercards() {
-    checklocalstorage()
+    let localStorageWached = localStorage.getItem('watchedFilms')
+    if (localStorageWached.length > 0) {
+        gallery.innerHTML = '';
+        const arrayLocalWachFilm = JSON.parse(localStorageWached)
+        console.log(arrayLocalWachFilm);
 
 
-}
+        for (const i of arrayLocalWachFilm) {
+           
+            const promis = getMovieDetails(i);
+            promis.then(
+                result => { 
+                    const imageUrl = result.data.poster_path
+            ? `https://image.tmdb.org/t/p/w500/${result.data.poster_path}`
+            : `${result.data.defaultPoster}`;
+                    const cardwachfil = `
+            <li class = "film_card" data-id="${i}">
+           <div class="film_card__img">
+           <img class="film_card__img--block"
+           src=${imageUrl}
+           alt="${result.data.original_title}">
+           </div>
+           <h3 class="film_card__title">${result.data.original_title}</h3>
+           <p class="film_card__type">Привет</p>
+           <p class="film_card__rating">Rating: ${result.data.vote_average}</p>
+           </li>
+           `
+                    gallery.insertAdjacentHTML('beforeend', cardwachfil)
+                    
+                    // console.log(result.data)
+                    console.log(result.data.poster_path)
+                    console.log(result.data.original_title)
+                    // console.log(result.data.original_name)
+                    // console.log(result.data.genre_ids)
+                    // console.log(result.data.first_air_date)
+                    console.log(result.data.vote_average)
+                    console.log(result.data.release_date)
+                
+
+                },
+               
+                        error => console.log(error)
+  );
+ 
+                    console.log('works')
+                } 
+            }
+        }
+    
+
+           
+                
+          
 
 
-function checklocalstorage(localStorageWached) {
-    console.log(localStorageWached)
+// function checklocalstorage(localStorageWached) {
+
+//     console.log(JSON.stringify(localStorageWached))
+//     console.log('no idea')
 
     
-}
+// }
 
 
 
 
-console.log(localStorageWached);
+// console.log(localStorageWached);
 
 
 
-let dataFilmsByWatched = [];
-let dataFilmsByQueue = [];
+// let dataFilmsByWatched = [];
+// let dataFilmsByQueue = [];
 
 // console.log(getFromStorage)
 
