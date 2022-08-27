ec2b6f82b6aa954cbc07771c409a4970ec41747b
Kirill Chaialo <fotonka098@gmail.com>
2022-08-27 17:54:41 +0300

    bugs-fix

--- src/js/header.js
+++ src/js/header.js
@@ -1,5 +1,3 @@
-
-
 const HEADER = document.querySelector('.header');
 const HEADERTHUMB = document.querySelector('.header-thumb');
 
@@ -12,15 +10,20 @@ const BTNCONTAINER = document.querySelector('.header-search__container');
 // LIBRARY.addEventListener('click', onLibraryClick);
 // HOME.addEventListener('click', onHomeClick);
 
+import backImage from '../images/library-desktop.jpg';
+import backImageTablet from '../images/library-tablet.jpg';
+import backImagePhone from '../images/library-phone.jpg';
+
+import mainBackImage from '../images/header-bg.jpg';
+
 function onLibraryClick() {
   INPCONTAINER.innerHTML = '';
   LIBRARY.classList.add('current');
   HOME.classList.remove('current');
   BTNCONTAINER.classList.remove('unvisible');
   HEADERTHUMB.classList.remove('hidden');
-  
+  HEADER.style.backgroundImage = `url(${backImage}`;
 }
-
 
 function onHomeClick() {
   INPCONTAINER.innerHTML = `<input
@@ -36,7 +39,23 @@ function onHomeClick() {
   LIBRARY.classList.remove('current');
   BTNCONTAINER.classList.add('unvisible');
   HEADERTHUMB.classList.add('hidden');
+  HEADER.style.backgroundImage = `url(${mainBackImage}`;
 }
 
-export { onLibraryClick }
-export { onHomeClick }
+if (window.matchMedia('(min-width: 1280px)').matches) {
+  HEADER.style.backgroundImage = `url(${backImageTablet}`;
+  return;
+}
+
+if (window.matchMedia('(min-width: 768px)').matches) {
+  HEADER.style.backgroundImage = `url(${backImagePhone}`;
+  return;
+}
+
+if (window.matchMedia('(min-width: 0px)').matches) {
+  HEADER.style.backgroundImage = `url(${backImage}`;
+  return;
+}
+
+export { onLibraryClick };
+export { onHomeClick };
